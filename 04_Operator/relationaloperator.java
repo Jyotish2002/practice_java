d5366c05297b085654001fa0f91dee574730808f
Jyotish <jyotishyadav09@gmail.com>
2025-09-11 01:46:20 +0530

    relational

---
+++ 04_Operator/relationaloperator.java
@@ -0,0 +1,8 @@
+public class relationaloperator {
+    public static void main(String[] args) {
+        //it return either true or false
+        System.out.println(1==2); //false
+        int a=20,b=24;
+        System.out.println(a!=b);//true
+    }
+}
