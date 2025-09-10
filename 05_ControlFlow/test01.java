8429feffc48a674e4effef085406f2e7a1226c51
Jyotish <jyotishyadav09@gmail.com>
2025-09-11 01:59:18 +0530

    control flow

---
+++ 05_ControlFlow/test01.java
@@ -0,0 +1,12 @@
+public class test01 {
+    public static void main(String[] args) {
+        String f1="Pratham",f2="Shubham",f3="Sumit",f4="Manish";
+        int a1=22,a2=21,a3=21,a4=22;
+      if(a1>a2 && a1>a3){
+        System.out.println(f1+" is elder than "+f2 +" and "+f3);
+      }
+     if(a1>a4 || a1==a4){
+        System.out.println(f1+" is same as "+f4);
+      }
+    }
+}
