101fb3831f0d3fda7096260b2e1ff7e6c4b0c7d4
Jyotish <jyotishyadav09@gmail.com>
2025-09-11 01:46:11 +0530

    Logical operator

---
+++ 04_Operator/logical.java
@@ -0,0 +1,7 @@
+public class logical {
+    public static void main(String[] args) {
+        int a=12,b=20;
+        System.out.println(a ==12 && b==10); //false
+        System.out.println(a ==12 || b==20); //true
+    }
+}
