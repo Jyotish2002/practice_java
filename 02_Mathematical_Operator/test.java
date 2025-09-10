8f88d0ddab487032dbb798c68b75df502bdd77c2
Jyotish <jyotishyadav09@gmail.com>
2025-09-10 22:37:23 +0530

    practice Java from day one 08/09

---
+++ 02_Mathematical_Operator/test.java
@@ -0,0 +1,24 @@
+public class test {
+    public static void main(String[] args) {
+        // float a= 10/3.0f;
+        // System.out.println(a);
+        // //remainder
+        // int b=10%3;
+        // System.out.println(b);
+        // int c = 10;
+        // c++; //post increment
+        // ++c; //pre increment
+        // c--; //post decrement
+        // --c; //pre decrement
+        // int d=12,e;
+        // e=++d;
+        // System.out.println(e);//13
+        // System.out.println(d);//13
+
+        int a=10;
+        System.out.print(a++);
+        System.out.print("+");
+        System.out.print(++a); //output will be 10 + 12
+
+    }
+}
