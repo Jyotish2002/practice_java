000e07409512edfa5eb8afcb0b25ca35b0ede744
Jyotish <jyotishyadav09@gmail.com>
2025-09-12 21:37:16 +0530

    loop

---
+++ 05_ControlFlow/test05.java
@@ -0,0 +1,16 @@
+public class test05 {
+    public static void main(String[] args){
+        int i=10;
+        // while(i<=100){
+        //     System.out.println("Hello");
+        //     i++;
+        // }
+        // for(i=0;i<100;){
+        //     System.out.println(i);
+        // }
+        // do { 
+        //     System.out.println("Do wala hu mei");
+        //     i++;
+        // } while (i<=100);tes
+    }
+}
