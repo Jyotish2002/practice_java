447f468f22a7c02abdbb418f54cc2be5aa1b0268
Jyotish <jyotishyadav09@gmail.com>
2025-09-12 21:13:07 +0530

    Advanced conditional statement

---
+++ 05_ControlFlow/test04.java
@@ -0,0 +1,7 @@
+public class test04 {
+    public static void main(String[] args){
+        int age=18;
+        String result = (age>=18 ) ? "You are good":"Not good";
+        System.out.println(result);
+    }
+}
